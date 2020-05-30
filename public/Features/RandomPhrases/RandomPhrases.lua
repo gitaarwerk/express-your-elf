a01528d4cd2a9db852ad6665b6bbfdc73056f1e2
Gitaarwerk <>
2020-05-30 17:22:33 +0200

    Fixed random phrases when no one is selected

--- public/Features/RandomPhrases/RandomPhrases.lua
+++ public/Features/RandomPhrases/RandomPhrases.lua
@@ -17,7 +17,6 @@ targetHisHer
 )
     local pickedPhrase
     local listOfPhrases = {
-
         "You no take candle.",
         "Knock knock",
         "I took an arrow to the knee.",
@@ -30,6 +29,7 @@ targetHisHer
 
     }
 
+    -- self
     if (playerRace == "Draenei" or playerRace == "Night Elf" or playerRace == "Tauren" or playerRace == "Highmountain Tauren" or playerRace == "Lightforged Draenei" or playerRace == "Blood Elf") then
         table.insert(listOfPhrases, "I always wanted to be taller.")
     end
@@ -43,17 +43,19 @@ targetHisHer
         table.insert(listOfPhrases, "Crowded elevators smell different to gnomes")
     end
 
-    if (targetLevel < 50) then
+    -- target
+    if (targetLevel and targetLevel < 50) then
         table.insert(listOfPhrases, 'I steal yo soul and cast Lightning level 1,000,000. Your body explodes into a fine bloody mist, because you are only a level ${targetLevel} ${targetClass}.')
     end
 
-    if (targetClass == "Warlock") then
-        table.insert(listOfPhrases, 'Uh, bing, bing, bong, bong,get out, demon!')
+    if (targetClass and targetClass == "Warlock") then
+        table.insert(listOfPhrases, 'Uh, bing, bing, bong, bong, get out, demon!')
     end
 
-    if (targetClass == "Priest") then
+    if (targetClass and targetClass == "Priest") then
         table.insert(listOfPhrases, "${targetName}, would you grab my arm, so I can tell my friends I’ve been touched by a Priest.")
     end
+
 
     pickedPhrase = listOfPhrases[math.random(1, #listOfPhrases)]
 
@@ -78,12 +80,14 @@ end
 
 
 function ExpressYourElf.RandomPhrases.run()
-    if(UnitName("target") and UnitPlayerControlled("target")) then
-        local playerName, playerGender, playerClass, playerRace, playerLevel  = ExpressYourElf.Helpers.GetPlayerInformation()
-        local targetName, targetGender, targetClass, targetRace, targetLevel  = ExpressYourElf.Helpers.GetTargetInformation()
+    local playerName, playerGender, playerClass, playerRace, playerLevel  = ExpressYourElf.Helpers.GetPlayerInformation()
+    local targetName, targetGender, targetClass, targetRace, targetLevel, targetHisHer
+    local playerHisHer = string.lower(ExpressYourElf.Helpers.GetHisHer(playerGender))
 
-        local playerHisHer = string.lower(ExpressYourElf.Helpers.GetHisHer(playerGender))
-        local targetHisHer = string.lower(ExpressYourElf.Helpers.GetHisHer(targetGender))
+    if(UnitName("target") and UnitPlayerControlled("target")) then
+        targetName, targetGender, targetClass, targetRace, targetLevel  = ExpressYourElf.Helpers.GetTargetInformation()
+        targetHisHer = string.lower(ExpressYourElf.Helpers.GetHisHer(targetGender))
+    end
 
     local RandomPhrase = ExpressYourElf.RandomPhrases.GetRandomMessage(
         playerName,
@@ -100,7 +104,5 @@ function ExpressYourElf.RandomPhrases.run()
         targetHisHer
     )
 
-        SendChatMessage(RandomPhrase, "SAY", nil, index);
-    end
+    SendChatMessage(RandomPhrase, "SAY", nil, index);
 end
-
