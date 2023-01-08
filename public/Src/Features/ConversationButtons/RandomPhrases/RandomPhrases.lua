57a1ed3dcb52af26f1676d909fd6cdba0560b745
Gitaarwerk <noreply@belastingdienst.nl>
2023-01-08 13:42:46 +0100

    add new random phrases

--- public/Src/Features/ConversationButtons/RandomPhrases/RandomPhrases.lua
+++ public/Src/Features/ConversationButtons/RandomPhrases/RandomPhrases.lua
@@ -54,6 +54,7 @@ playerSirMam
         "It’s happening again!",
         "My nipple is broken.",
         "My hair hurts.",
+        "I’ve never seen the inside of my own mouth because it scares me to death.",
         "If it were for the kobolds, we’d all be watching TV by candlelight.",
         "My eye socket is warm.",
         "Have you ever tried sleeping in water?",
@@ -69,6 +70,8 @@ playerSirMam
         "I think the only difference between me and other guildies is that I'm more honest and my ${oppositeSex} are more beautiful.",
         "I saved a big war. I saved a couple of them.",
         "Have you seen the outrageous prices here?",
+        "I look very much forward to showing my financials, because they are huge.",
+        "Arthas speaks and his people sit up at attention. I want my people to do the same.",
         "I am the only one who can make ${randomWorld} truly great again!",
         'He calls me Mr. ${playerRace}. I’ve known this guy forever. He used to call me, "Hey, ${playerName}, let’s go out to dinner."s You know, before, he’ll go, "Hey, let’s go out to dinner." Now he goes, "Mr. ${playerRace}, ${playerSirMam}, how are you?"',
         'No, no. But I don`t say -- I say flames, we`ll put out the flames. And we`ll put out in some cases just burning embers. We also have burning embers. We have embers and we do have flames. Mount Hyjal became more flame like, but it`s going to be under control.',
@@ -84,6 +87,10 @@ playerSirMam
         'Yes, I am an agent of the Legion, but my duties are largely ceremonial.',
         'Did you know my powers can only be used for good.',
         'I don’t work here. I’m a consultant.',
+        "I didn’t need this. I had a nice easy life.",
+        "I’m a victim. I will tell you.",
+        "Before, I was arrogant, but now I'm perfect.",
+        "Prior to the plague coming in, the people were calling me. ...You wouldn’t believe it, people that were so far gone, I figured they’d never speak to me and I would never speak to them.",
         'My toys! My toys! I can’t do this job without my toys!',
         'It might look like I’m doing nothing, but at the cellular level I’m really quite busy.',
         'At least I have a positive attitude about my destructive habits.',
@@ -96,6 +103,20 @@ playerSirMam
         'Please don’t tell my Arthas that I used his 2008 Frostmourne to destroy the town of Gilneas, Azeroth in 2010',
         "Those warriors from the Maw? They've got curved swords. Curved. Swords.",
         "Goblin wind turbines kill 13-39 million birds and bats every year!",
+        "I tried to be normal once… worst two minutes of my entire life.",
+        "I like to be an example for others. Usually a bad example, though.",
+        "Common sense is like deodorant—the people who need it most never seem to use it.",
+        "I’d like to help you out today, which way did you come in?",
+        "Take my advice—I’m not using it.",
+        "I used to think I was indecisive, but now I’m not really sure",
+        "I respect the opinion of everyone who agrees with me.",
+        "Here I am! What are your other two wishes?",
+        "I'm not as confused as I seem to act.",
+        "Sometimes I just wish dragons would abduct me and crown me their leader.",
+        "My parents moved around a lot when I was growing up, but I always found them.",
+        "I just bought this amazing chicken broth at the stock market.",
+        "And yet I’ve gone decades, decades without a war. The first ${playerRace} to do it for that long a period.",
+        "When you see the other side chopping off heads, waterboarding doesn't sound very severe.",
         "When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love, to loot epics and respawn.",
         "We’re rounding ’em up in a very ${playerRace} way, in a very nice way. And they’re going to be happy because they want to be legalized. And, by the way, I know it doesn’t sound nice. But not everything is nice.",
         'How was the weekend, someone asked? Well, on my first date, ${oppositHeShe} said, “I want to take you to the cemetery to meet my parents, you might be the one.”'
@@ -104,6 +125,10 @@ playerSirMam
     -- self
     if (playerRace == "Draenei" or playerRace == "Night Elf" or playerRace == "Tauren" or playerRace == "Highmountain Tauren" or playerRace == "Lightforged Draenei" or playerRace == "Blood Elf") then
         table.insert(listOfPhrases, "I always wanted to be taller.");
+    end
+
+    if (not(playerRace == "Dracthyr")) then 
+        table.insert(listOfPhrases, "What I won't do is take in two hundred thousand dragons who could be Dracthyr... I have been watching this migration. And I see the people. I mean, they're dragons. They're mostly dragons, and they're strong dragons. These are physically young, strong dragons. They look like prime-time soldiers. Now it's probably not true, but where are the women?... So, you ask two things. Number one, why aren't they fighting for their country? And number two, I don't want these dragons coming over here.")
     end
 
     if (playerGender == "male") then 
@@ -122,6 +147,7 @@ playerSirMam
 
     
     if (playerRace == "Dracthyr") then
+        table.insert(listOfPhrases, "I have scales, does that mean I'm actually a fish?")
         table.insert(listOfPhrases, "Watch out, here be dragons!");
         table.insert(listOfPhrases, "Yesterday, I was telling a bedtime fantastical story to my little son. The story kept dragon-on and on and on!");
         table.insert(listOfPhrases, "Did you know that I am an extremely good guitarists. I really know all the scales!");
@@ -130,6 +156,7 @@ playerSirMam
         
         if (playerGender == "female") then
             table.insert(listOfPhrases, "Did you know, that I actualy prefer to wash my scales with Head and Smolders?");
+            table.insert(listOfPhrases, "Dragons girls wear bows and bling, because it is a dragons thing.");
         end
     end
 
@@ -211,26 +238,42 @@ targetGender,
 targetClass,
 targetRace,
 targetLevel,
-targetHisHer
+targetHisHer,
+targetHeShe,
+targetHimHer
 )
     local pickedPhrase;
     local listOfPhrases = {
-        "Saw ${targetName}, wanted it, bought it, used it once, kept it in my house for ten years, gave it away."
+        "Despite the outcome in the guild, we cannot lose hope and we must all work very hard for a friend and a great person named ${targetName}.",
+        "Saw ${targetName}, wanted it, bought it, used it once, kept it in my house for ten years, gave it away.",
+        "Hey ${targetName}, your right cheek looks very well today!",
+        "I had lunch with ${targetName} last night.",
+        "OH NO!, ${targetName}'s at it again!",
+        "${targetName}, I regret to inform you that you are no longer welcome at The Knights of The Twisted Knee.",
+        "Now, ${targetName}, let’s talk about why I am bitter.",
+        "Do you see ${targetName} over there? I think ${targetHeShe}'s my family.",
+        "${targetName} has many unpopular opinions. I believe ${targetHisHer} most unpopular opinions are usually about me.",
+        "I wonder if I ever appeared in ${targetName}'s dream.",
+        "${targetName} often lies on the floor.",
+        "${targetName}, I think I'm here for you.",
+        "Here is the sitting-cream back, ${targetName}. It was already half-empty.",
+        "Have you read the book 'How to deal with awkward situations'? I borrowed it from ${targetName}.",
+        "What I'm doing is NOT creepy, I am just taking notes on every move ${targetName} makes.",
+        "I think I just hugged my dentist instead of ${targetName}.",
+        "Can someone take a picture of ${targetName} and me? Because these two buddies are looking AWE-SOME!",
+        "I have ${targetName}’s hand tattooed on my shoulder, wanna see it?",
+        "My life’s goal is to convince my partner to cover ${targetName}, dressed as a trendy ${targetRace}.",
+        "I love reading the 10 weirdest things ${targetName} said.",
+        "This is ${targetName}, ${targetHeShe} likes to listen to stories of people and ${targetHeShe} always smiles. Hope you take good care of ${targetHimHer}",
+        "${targetName}, You must go forth into the world, with passion, courage in your conviction, and most importantly be true to yourself. I did it!",
+        "${targetName} is somebody that I've always liked, but a lot of people like ${targetName}. Some people probably don't like ${targetName}, but ${targetName}'s somebody I've always liked.",
+        "I have tremendous respect for ${targetRace}s.",
+        "I name all of my children to important people in my life. My last one is called ${targetName}.",
+        "People like the Pandaren, abusing their dual factionship and smuggling in illegal ${targetRace} among other disgusting deplorables, how sad their lives must be."
     }
 
-    -- target
-    if (targetName) then
-        table.insert(listOfPhrases, "${targetName}, You must go forth into the world, with passion, courage in your conviction, and most importantly be true to yourself. I did it!");
-        table.insert(listOfPhrases, "${targetName} is somebody that I've always liked, but a lot of people like ${targetName}. Some people probably don't like ${targetName}, but ${targetName}'s somebody I've always liked.");
-    end
-
-    if (targetName == "Yamisuzu" and targetName == "Alhena") then
+    if (targetName == "Yamisuzu" or targetName == "Alhena") then
         table.insert(listOfPhrases, "ally ally ally! OI OI OI!");
-    end
-
-    if (targetRace) then
-        table.insert(listOfPhrases, "People like the Pandaren, abusing their dual factionship and smuggling in illegal ${targetRace} among other disgusting deplorables, how sad their lives must be.");
-        table.insert(listOfPhrases, "I have tremendous respect for ${targetRace}s.");
     end
 
     if (targetLevel and targetLevel < 50) then
@@ -250,7 +293,7 @@ targetHisHer
     end
 
     if (playerRace == "Dracthyr" and targetClass == "Paladin") then
-        table.insert(rudeLines, "I don't eat paladins because they always taste lawful!");
+        table.insert(listOfPhrases, "I don't eat paladins because they always taste lawful!");
     end
 
 
@@ -271,7 +314,9 @@ targetHisHer
             targetClass = targetClass,
             targetRace = targetRace,
             targetLevel = targetLevel,
-            targetHisHer = targetHisHer
+            targetHisHer = targetHisHer,
+            targetHeShe = targetHeShe,
+            targetHimHer = targetHimHer,
         }
     )
 end
@@ -286,6 +331,8 @@ function ExpressYourElf.RandomPhrases.run()
     if(UnitName("target") and UnitPlayerControlled("target")) then
         targetName, targetGender, targetClass, targetRace, targetLevel  = ExpressYourElf.Helpers.GetTargetInformation()
         targetHisHer = string.lower(ExpressYourElf.Helpers.GetHisHer(targetGender))
+        targetHeShe = string.lower(ExpressYourElf.Helpers.GetHeShe(targetGender))
+        targetHimHer = string.lower(ExpressYourElf.Helpers.GetHimHer(targetGender))
     end
 
     -- get random phrases
@@ -303,7 +350,9 @@ function ExpressYourElf.RandomPhrases.run()
             targetClass,
             targetRace,
             targetLevel,
-            targetHisHer
+            targetHisHer,
+            targetHeShe,
+            targetHimHer
         )
         SendChatMessage(RandomPhraseTargeted);
     else
